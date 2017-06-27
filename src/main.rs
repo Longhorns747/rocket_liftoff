34130fd0a6576efa20d694d43ca8a16d0fc674a7
longhorns747 <longhorns747@users.noreply.github.com>
2017-06-26 21:24:44 -0700

    Messing with nested structs

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,25 @@ use rocket_contrib::Template;
 #[derive(Serialize)]
 struct Website {
     title: String,
-    mytext: String
+    body: WebsiteBody
+}
+
+#[derive(Serialize)]
+struct WebsiteBody {
+    body_text_h1: String,
+    body_text_h2: String
 }
 
 #[get("/")]
 fn index() -> Template {
+    let body_content = WebsiteBody {
+        body_text_h1: String::from("Hello!"),
+        body_text_h2: String::from("World!")
+    };
+
     let context = Website {
         title: String::from("Ready to launch!"),
-        mytext: String::from("Whoa!")
+        body: body_content
     };
 
     Template::render("index", &context)
